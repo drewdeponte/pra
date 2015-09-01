fcf75881ad520a82ba5ea5ee85a9539ea23c453e
Andrew De Ponte <cyphactor@gmail.com>
2015-09-01 14:27:51 -0700

    Make Log and ErrorLog properly output timestamps
    Why you made the change:
    
    I did this so that the logging would be more valuable in terms of
    understanding when things happened.
    
    How the change addresses the need:
    
    Adding timestamps helps provide a reference to time of when things
    occurred.

--- lib/pra/error_log.rb
+++ lib/pra/error_log.rb
@@ -1,10 +1,11 @@
 require 'pra/config'
+require 'date'
 
 module Pra
   class ErrorLog
     def self.log(error)
       File.open(Pra::Config.error_log_path, 'a') do |f|
-        f.puts(error.message)
+        f.puts("#{DateTime.now.iso8601} - #{error.message}")
         error.backtrace.each { |line| f.puts(line) }
       end
     end
