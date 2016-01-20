72086e5a3e034bdc008f40f44ed5e8abe2967e93
Brian Miller <brian@acorns.com>
2016-01-20 08:07:11 -0800

    Added search by organization and repo exclusions

--- lib/pra/github_pull_source.rb
+++ lib/pra/github_pull_source.rb
@@ -21,20 +21,12 @@ module Pra
     def get_all_pull_requests
       pull_requests = []
       pull_requests_json = "[]"
-      q_repos = ""
-      @config['repositories'].each do |repo|
-        if q_repos.empty?
-          q_repos << "repo:#{repo['owner']}/#{repo['repository']}"
-        else
-          q_repos << " repo:#{repo['owner']}/#{repo['repository']}"
-        end
-      end
 
       conn = Faraday.new
       conn.basic_auth(@config['username'], @config['password'])
       resp = conn.get do |req|
         req.url rest_api_search_issues_url
-        req.params['q'] = "is:pr is:open sort:updated-desc #{q_repos}"
+        req.params['q'] = "is:pr is:open sort:updated-desc #{repos_for_query}"
         req.headers['Content-Type'] = 'application/json'
         req.headers['Accept'] = 'application/json'
       end
@@ -51,14 +43,17 @@ module Pra
       Pra::Log.log(pull_requests_hash.inspect)
       pull_requests_hash['items'].each do |request|
         begin
-          pull_requests << Pra::PullRequest.new(title: request["title"],
-                                           from_reference: "",
-                                           to_reference: "",
-                                           author: request["user"]["login"],
-                                           assignee: request["assignee"] ? request["assignee"]["login"] : nil,
-                                           link: request['html_url'],
-                                           service_id: 'github',
-                                           repository: extract_repository_from_html_url(request['html_url']))
+          org, repository = extract_repository_from_html_url(request['html_url'])
+          unless excluded?(org, repository)
+            pull_requests << Pra::PullRequest.new(title: request["title"],
+                                                  from_reference: "",
+                                                  to_reference: "",
+                                                  author: request["user"]["login"],
+                                                  assignee: request["assignee"] ? request["assignee"]["login"] : nil,
+                                                  link: request['html_url'],
+                                                  service_id: 'github',
+                                                  repository: repository)
+          end
         rescue StandardError => e
           Pra::Log.log("Error: #{e.to_s}")
           Pra::Log.log("Request: #{request.inspect}")
@@ -68,9 +63,40 @@ module Pra
       pull_requests
     end
 
+    def repos_for_query
+      query_params = [] 
+      repositories.each do |repo|
+        query_params << "repo:#{repo['owner']}/#{repo['repository']}"
+      end
+
+      @excluded_repos = {} 
+      organizations.each do |org|
+        query_params << "org:#{org['name']}"
+        @excluded_repos[org['name'].downcase] = org['exclude']
+      end
+
+      return query_params.join(" ")
+    end
+
+    def excluded_repos
+      @excluded_repos || collect_exclusions
+    end
+
+    def collect_exclusions
+      @exclusions = {}
+      organizations.each do |org|
+        @exclusions[org['name'].downcase] = org['exclude']
+      end
+      @exclusions
+    end
+
+    def excluded?(org, repository)
+      excluded_repos[org.downcase] && excluded_repos[org.downcase].include?(repository)
+    end
+
     def extract_repository_from_html_url(html_url)
-      /https:\/\/github.com\/\w+\/([\w-]+)/.match(html_url)
-      return $1
+      /https:\/\/github.com\/(\w+)\/([\w-]+)/.match(html_url)
+      return $1, $2
     end
 
     def rest_api_search_issues_url
@@ -78,7 +104,11 @@ module Pra
     end
 
     def repositories
-      @config["repositories"]
+      @config["repositories"] || []
+    end
+
+    def organizations
+      @config["organizations"] || []
     end
 
     def get_repo_pull_requests(repository_config)
